5177b44fd8e2024a28c2d84d14e77b39e45146cb
Dhanuxeptagon <dhanushkapeiris@xeptagon.com>
2023-05-19 22:05:33 +0530

    fix commit error

--- src/components/TestComponent/testComponent.tsx
+++ src/components/TestComponent/testComponent.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react'
 
 export const TestComponent = () => {
-  const [name,setName] = useState('');
-  setName('test Component');
+  const [name] = useState('test Component');
   return (
     <div className='test'>name from state {name}</div>
   )
