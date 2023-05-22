b8701e03e8dd4b66bffb9f2f69e459cbc3c211e9
Dhanuxeptagon <dhanushkapeiris@xeptagon.com>
2023-05-22 07:27:30 +0530

    fixed scss issue

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,9 @@
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss";
 
 const packageJson = require("./package.json");
 
@@ -21,14 +23,17 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
+    external: [/\.(sass|scss|css)$/] /* ignore style files */,
     plugins: [dts.default()],
   }
 ];
--- src/components/TestComponent/testComponent.tsx
+++ src/components/TestComponent/testComponent.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 export const TestComponent = () => {
   const [name] = useState('test Component');
   return (
-    <div className='test'>name from state {name}</div>
+    <div className="green">name from state {name}</div>
   )
 }
 
