abbbd667f3235cbde7f2fae43278e3c8c92e4690
Dhanuxeptagon <dhanushkapeiris@xeptagon.com>
2023-05-29 16:55:43 +0530

    changed the folder sturture

--- web/dist/cjs/types/components/User/UserManagement/userManagementComponent.d.ts
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import "./userManagementComponent.scss";
-import "../../../Styles/common.table.scss";
-export declare const UserManagementComponent: (props: any) => React.JSX.Element;
--- web/dist/esm/types/components/Common/OrganisationStatus/organisationStatus.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './organisationStatus.scss';
-export interface OrganisationStatusProps {
-    organisationStatus: number;
-    t: any;
-}
-declare const OrganisationStatus: (props: OrganisationStatusProps) => React.JSX.Element;
-export default OrganisationStatus;
