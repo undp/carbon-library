c1fc70f32d1f68576e5b1b591b18eb3a06a14573
Yathurshan Thurairajah <yathurshant@xeptagon.com>
2023-06-01 13:55:59 +0530

    Style model fixes

--- src/Components/Common/Models/changePasswordModel.tsx
+++ src/Components/Common/Models/changePasswordModel.tsx
@@ -27,110 +27,110 @@ const ChangePasswordModel: FC<ChangePasswordProps> = (
   } = props;
 
   return (
-    <div className="antd-model-styles-overwrites">
-      <Modal
-        width={450}
-        title={
-          <div className="popup-header">
-            <div className="icon">
-              <Lock size={100} color="#16B1FF" />
-            </div>
-            <div>{t("passwordReset:changePassword")}</div>
+    <Modal
+      width={450}
+      title={
+        <div className="popup-header">
+          <div className="icon">
+            <Lock size={100} color="#16B1FF" />
           </div>
-        }
-        open={openModal}
-        className={"popup-success password-reset-model"}
-        centered={true}
-        destroyOnClose={true}
-        footer={null}
-        onCancel={onCanceled}
+          <div>{t("passwordReset:changePassword")}</div>
+        </div>
+      }
+      open={openModal}
+      className={
+        "popup-success password-reset-model antd-model-styles-overwrites"
+      }
+      centered={true}
+      destroyOnClose={true}
+      footer={null}
+      onCancel={onCanceled}
+    >
+      <Form
+        name="change_password"
+        layout="vertical"
+        className="login-form"
+        onFieldsChange={onFieldsChanged}
+        onFinish={onPasswordChanged}
       >
-        <Form
-          name="change_password"
-          layout="vertical"
-          className="login-form"
-          onFieldsChange={onFieldsChanged}
-          onFinish={onPasswordChanged}
+        <Form.Item
+          className="mg-top-1"
+          name="oldPassword"
+          label={t("passwordReset:oldPassword")}
+          rules={[
+            {
+              required: true,
+              message: `${t("passwordReset:oldPassword")} ${t(
+                "passwordReset:isRequired"
+              )}`,
+            },
+          ]}
         >
-          <Form.Item
-            className="mg-top-1"
-            name="oldPassword"
-            label={t("passwordReset:oldPassword")}
-            rules={[
-              {
-                required: true,
-                message: `${t("passwordReset:oldPassword")} ${t(
-                  "passwordReset:isRequired"
-                )}`,
+          <Input.Password placeholder="" />
+        </Form.Item>
+
+        <Form.Item
+          name="newPassword"
+          label={t("passwordReset:newPassword")}
+          rules={[
+            {
+              required: true,
+              message: `${t("passwordReset:newPassword")} ${t(
+                "passwordReset:isRequired"
+              )}`,
+            },
+          ]}
+        >
+          <Input.Password placeholder="" />
+        </Form.Item>
+
+        <Form.Item
+          name="confirm_password"
+          label={t("passwordReset:confirmNewPassword")}
+          dependencies={["newPassword"]}
+          rules={[
+            {
+              required: true,
+              message: `${t("passwordReset:confirmNewPassword")} ${t(
+                "passwordReset:isRequired"
+              )}`,
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue("newPassword") === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(
+                  new Error(
+                    t("passwordReset:passwordsNotMatchedErr").toString()
+                  )
+                );
               },
-            ]}
+            }),
+          ]}
+        >
+          <Input.Password placeholder="" />
+        </Form.Item>
+
+        {errorMsg && (
+          <Alert className="error" message={errorMsg} type="error" showIcon />
+        )}
+
+        <div className="mg-top-2 ant-modal-footer">
+          <Button htmlType="button" onClick={onCanceled}>
+            {t("passwordReset:cancel")}
+          </Button>
+          <Button
+            className="mg-left-2"
+            type="primary"
+            htmlType="submit"
+            loading={loadingBtn}
           >
-            <Input.Password placeholder="" />
-          </Form.Item>
-
-          <Form.Item
-            name="newPassword"
-            label={t("passwordReset:newPassword")}
-            rules={[
-              {
-                required: true,
-                message: `${t("passwordReset:newPassword")} ${t(
-                  "passwordReset:isRequired"
-                )}`,
-              },
-            ]}
-          >
-            <Input.Password placeholder="" />
-          </Form.Item>
-
-          <Form.Item
-            name="confirm_password"
-            label={t("passwordReset:confirmNewPassword")}
-            dependencies={["newPassword"]}
-            rules={[
-              {
-                required: true,
-                message: `${t("passwordReset:confirmNewPassword")} ${t(
-                  "passwordReset:isRequired"
-                )}`,
-              },
-              ({ getFieldValue }) => ({
-                validator(_, value) {
-                  if (!value || getFieldValue("newPassword") === value) {
-                    return Promise.resolve();
-                  }
-                  return Promise.reject(
-                    new Error(
-                      t("passwordReset:passwordsNotMatchedErr").toString()
-                    )
-                  );
-                },
-              }),
-            ]}
-          >
-            <Input.Password placeholder="" />
-          </Form.Item>
-
-          {errorMsg && (
-            <Alert className="error" message={errorMsg} type="error" showIcon />
-          )}
-
-          <div className="mg-top-2 ant-modal-footer">
-            <Button htmlType="button" onClick={onCanceled}>
-              {t("passwordReset:cancel")}
-            </Button>
-            <Button
-              className="mg-left-2"
-              type="primary"
-              htmlType="submit"
-              loading={loadingBtn}
-            >
-              {t("passwordReset:setPassword")}
-            </Button>
-          </div>
-        </Form>
-      </Modal>
-    </div>
+            {t("passwordReset:setPassword")}
+          </Button>
+        </div>
+      </Form>
+    </Modal>
   );
 };
 
--- src/Components/Common/Models/userActionConfirmationModel.tsx
+++ src/Components/Common/Models/userActionConfirmationModel.tsx
@@ -33,75 +33,73 @@ const UserActionConfirmationModel: FC<UserActionProps> = (
   }, [openModal]);
 
   return (
-    <div className="antd-model-styles-overwrites">
-      <Modal
-        title={
-          <div className="popup-header">
-            <div className="icon">{actionInfo.icon}</div>
-            <div>{actionInfo.headerText}</div>
-          </div>
-        }
-        className={"popup-" + actionInfo.type}
-        open={openModal}
-        width={Math.min(400, window.innerWidth)}
-        centered={true}
-        onCancel={onActionCanceled}
-        destroyOnClose={true}
-        footer={null}
+    <Modal
+      title={
+        <div className="popup-header">
+          <div className="icon">{actionInfo.icon}</div>
+          <div>{actionInfo.headerText}</div>
+        </div>
+      }
+      className={"popup-" + actionInfo.type + " antd-model-styles-overwrites"}
+      open={openModal}
+      width={Math.min(400, window.innerWidth)}
+      centered={true}
+      onCancel={onActionCanceled}
+      destroyOnClose={true}
+      footer={null}
+    >
+      <p style={{ whiteSpace: "pre-line" }}>{actionInfo.text}</p>
+      <Form
+        layout="vertical"
+        onFinish={() => {
+          onActionConfirmed(comment);
+        }}
       >
-        <p style={{ whiteSpace: "pre-line" }}>{actionInfo.text}</p>
-        <Form
-          layout="vertical"
-          onFinish={() => {
-            onActionConfirmed(comment);
-          }}
+        <Form.Item
+          className="mg-bottom-0"
+          label={t("userProfile:remarks")}
+          name="remarks"
+          required={actionInfo.type === "danger"}
         >
-          <Form.Item
-            className="mg-bottom-0"
-            label={t("userProfile:remarks")}
-            name="remarks"
-            required={actionInfo.type === "danger"}
+          <TextArea
+            defaultValue={comment}
+            rows={2}
+            onChange={(v) => setComment(v.target.value)}
+          />
+        </Form.Item>
+
+        {errorMsg ? (
+          <Alert
+            className="mg-top-1"
+            message={errorMsg}
+            type="error"
+            showIcon
+          />
+        ) : (
+          ""
+        )}
+
+        <div className="mg-top-1 ant-modal-footer padding-bottom-0">
+          <Button
+            htmlType="button"
+            onClick={() => {
+              onActionCanceled();
+            }}
           >
-            <TextArea
-              defaultValue={comment}
-              rows={2}
-              onChange={(v) => setComment(v.target.value)}
-            />
-          </Form.Item>
-
-          {errorMsg ? (
-            <Alert
-              className="mg-top-1"
-              message={errorMsg}
-              type="error"
-              showIcon
-            />
-          ) : (
-            ""
-          )}
-
-          <div className="mg-top-1 ant-modal-footer padding-bottom-0">
-            <Button
-              htmlType="button"
-              onClick={() => {
-                onActionCanceled();
-              }}
-            >
-              {t("userProfile:cancel")}
-            </Button>
-            <Button
-              className="mg-left-2"
-              type="primary"
-              htmlType="submit"
-              loading={loading}
-              disabled={actionInfo.type === "danger" && comment === ""}
-            >
-              {actionInfo.action}
-            </Button>
-          </div>
-        </Form>
-      </Modal>
-    </div>
+            {t("userProfile:cancel")}
+          </Button>
+          <Button
+            className="mg-left-2"
+            type="primary"
+            htmlType="submit"
+            loading={loading}
+            disabled={actionInfo.type === "danger" && comment === ""}
+          >
+            {actionInfo.action}
+          </Button>
+        </div>
+      </Form>
+    </Modal>
   );
 };
 
