0337f7714ad5571279f8b914f8e370cda26afc25
palindaa <palinda@xeptagon.com>
2023-09-08 15:01:16 +0530

    Change AWS SDK

--- backend/carbon-services-lib/src/shared/file-handler/s3.filehandler.service.ts
+++ backend/carbon-services-lib/src/shared/file-handler/s3.filehandler.service.ts
@@ -1,12 +1,12 @@
 import { Injectable, Logger } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { FileHandlerInterface } from "./filehandler.interface";
-var AWS = require("aws-sdk");
-const s3 = new AWS.S3();
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 
 @Injectable()
 export class S3FileHandlerService implements FileHandlerInterface {
 
+  private client = new S3Client({})
   constructor(
     private logger: Logger,
     private configService: ConfigService
@@ -14,19 +14,31 @@ export class S3FileHandlerService implements FileHandlerInterface {
   
   public async uploadFile(path: string, content: string): Promise<string> {
     const imgBuffer = Buffer.from(content, "base64");
-    var uploadParams = {
+    // var uploadParams = {
+    //   Bucket: this.configService.get<string>("s3CommonBucket.name"),
+    //   Key: "",
+    //   Body: imgBuffer,
+    //   ContentEncoding: "base64",
+    //   ContentType: "image/png",
+    // };
+    
+    const putCommand = new PutObjectCommand({
       Bucket: this.configService.get<string>("s3CommonBucket.name"),
-      Key: "",
+      Key: path,
       Body: imgBuffer,
       ContentEncoding: "base64",
       ContentType: "image/png",
-    };
+    })
+    const resp = await this.client.send(putCommand)
+
+    const encodeFileName = encodeURIComponent(path);
+    return `https://${this.configService.get<string>("s3CommonBucket.name")}.s3.amazonaws.com/${encodeFileName}`
     
     // uploadParams.Key = `profile_images/${companyId}_${new Date().getTime()}.png`;
-    uploadParams.Key = path
-    return (await s3
-      .upload(uploadParams)
-      .promise())?.Location;
+    // uploadParams.Key = path
+    // return (await s3
+    //   .upload(uploadParams)
+    //   .promise())?.Location;
   }
 
 
