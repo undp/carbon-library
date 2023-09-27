91038059984bf37dd23833d2b48fbddde1066a2e
Dhanuxeptagon <dhanushkapeiris@xeptagon.com>
2023-09-27 17:06:03 +0530

    password hash changes

--- backend/carbon-services-lib/src/shared/configuration.ts
+++ backend/carbon-services-lib/src/shared/configuration.ts
@@ -22,6 +22,7 @@ export default () => ({
     expiresIn: process.env.EXPIRES_IN || "7200",
     userSecret: process.env.USER_JWT_SECRET || "1324",
     adminSecret: process.env.ADMIN_JWT_SECRET || "8654",
+    encodePassword: process.env.ENCODE_PASSWORD || false
   },
   ledger: {
     name: "carbon-registry-" + (process.env.NODE_ENV || "dev"),
