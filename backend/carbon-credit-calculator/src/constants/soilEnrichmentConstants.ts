c5ceb8d8fc49d54007c55f7fcaade9f3b7b48d66
tharindugayanga <tharindug@xeptagon.com>
2023-10-12 11:59:15 +0530

    code refactor

--- backend/carbon-credit-calculator/src/constants/soilEnrichmentConstants.ts
+++ backend/carbon-credit-calculator/src/constants/soilEnrichmentConstants.ts
@@ -2,5 +2,5 @@ import { SubSectorConstants } from "./subSectorConstants";
 
 export class SoilEnrichmentConstants implements SubSectorConstants {
     emissionFactor: number = 3.66;
-    emissionFactorUnit: string = "tCO2e";
+    emissionFactorUnit: string = "tCO2e"; // emission factor
 }
