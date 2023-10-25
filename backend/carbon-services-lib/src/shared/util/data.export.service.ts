27a76f65ce566d0f2085ad9e80be3821e8f66bea
tharindugayanga <tharindug@xeptagon.com>
2023-10-25 07:21:06 +0530

    CARBON-325 code refactor

--- backend/carbon-services-lib/src/shared/util/data.export.service.ts
+++ backend/carbon-services-lib/src/shared/util/data.export.service.ts
@@ -1,78 +1,40 @@
 import { Injectable } from "@nestjs/common";
 import * as fs from 'fs';
-import { FileHandlerInterface } from '../file-handler/filehandler.interface'; 
+import { FileHandlerInterface } from '../file-handler/filehandler.interface';
 import { DataExportDto } from "../dto/data.export.dto";
 
 @Injectable()
 export class DataExportService {
-    constructor(private fileHandler: FileHandlerInterface,) {
+  constructor(private fileHandler: FileHandlerInterface,) {
 
-    };
+  };
 
-    async generateCsv(data: DataExportDto[], headers: string[]) {
+  async generateCsv(data: DataExportDto[], headers: string[]) {
 
-        const csvFile = 'data_'+ Date.now() +'.csv';
-        //   const ws = fs.createWriteStream(csvFile);
-        //   const csvStream = fastcsv.format({ headers: true });
-        //   csvStream.pipe(ws);
-      
-        //   // Write the data to the stream
-        //   programmes.forEach((data) => csvStream.write(data));
-      
-        //   // End the stream to signal that writing is complete
-        //   csvStream.end();
+    const csvFile = 'data_' + Date.now() + '.csv';
 
-        console.log('========================DataExportService-prepdata', data);
-          
-        let csvContent = ''
-        // // const stream = fs.createWriteStream('/tmp/' + filePath);
-        // // csvContent.pipe()
+    let csvContent = ''
 
-        const refinedData = []
-        
-        refinedData.push(headers)
-        
-        data.forEach(item => {
-          refinedData.push(Object.values(item))  
-        })
-        
-        
+    const refinedData = [];
+    refinedData.push(headers);
 
-        
-        refinedData.forEach(row => {
-          csvContent += row.join(',') + '\n'
-        })
-        
-        // console.log('========================csvContent', filePath);
-          // Wait for the finish event before proceeding with the deletion
-        //   return new Promise<string>((resolve, reject) => {
-        //     ws.on('finish', () => {
-        //       // Now that writing is finished, delete the file
-        //     //   fs.unlinkSync(filePath);
-        //       resolve(filePath);
-              
-        //     });
-        //     ws.on('error', (err) => {
-        //       reject(err);
-        //     });
-        //   });
+    data.forEach(item => {
+      const values = Object.values(item).map(value => (value === undefined || value === null) ? "" : value);
+      refinedData.push(values);
+    });
 
 
-        // console.log('========================ws', ws);
-        // const content = await new Promise<string>(resolve => {
-        //     writer.on("finish", function() {
-        //         const contents = fs.readFileSync(csvFile, {encoding: 'base64'})
-        //         resolve(contents);
-        //     });
-        // });
+    refinedData.forEach(row => {
+      const rowValues = row.map(value => `"${value}"`).join(',');
+      csvContent += rowValues + '\n';
+    });
 
-        fs.writeFileSync(csvFile, csvContent);
-        const content = fs.readFileSync(csvFile, {encoding: 'base64'});
-        console.log('========================content333', content);
-          const url = await this.fileHandler.uploadFile('documents/exports/' + csvFile, content);
-          console.log('========================PDF generate end', 'exports/', url);
-          return {url, csvFile};
-      }
+    fs.writeFileSync(csvFile, csvContent);
+    const content = fs.readFileSync(csvFile, { encoding: 'base64' });
+    const url = await this.fileHandler.uploadFile('documents/exports/' + csvFile, content);
+    console.log('========================PDF generate end', 'exports/', url);
+    return { url, csvFile };
+  }
 }
 
 
