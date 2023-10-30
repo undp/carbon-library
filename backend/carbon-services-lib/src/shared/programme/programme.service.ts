0b6472652a0d9e10e2719e9fb904ac198533d54a
Dhanuxeptagon <dhanushkapeiris@xeptagon.com>
2023-10-30 16:48:34 +0530

    added ndcDetailManagementComponent

--- backend/carbon-services-lib/src/shared/programme/programme.service.ts
+++ backend/carbon-services-lib/src/shared/programme/programme.service.ts
@@ -1884,6 +1884,16 @@ export class ProgrammeService {
     return new DataResponseDto(HttpStatus.OK, saved);
   }
 
+  async queryNdcDetails(
+    query: QueryDto,
+    abilityCondition: string
+  ): Promise<DataListResponseDto> {
+    return new DataListResponseDto(
+      undefined,
+      undefined
+    );
+  }
+
   async queryNdcActions(
     query: QueryDto,
     abilityCondition: string
