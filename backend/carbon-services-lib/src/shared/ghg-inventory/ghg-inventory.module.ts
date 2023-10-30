46ed39603e663cc8d37bddc764a841342ba733ce
tharindugayanga <tharindug@xeptagon.com>
2023-10-30 15:14:12 +0530

    refactor file structure

--- backend/carbon-services-lib/src/shared/ghg-inventory/ghg-inventory.module.ts
+++ backend/carbon-services-lib/src/shared/ghg-inventory/ghg-inventory.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GhgEmissionsService } from './ghg-emissions/ghg-emissions.service';
-import { GhgProjectionsService } from './ghg-projections.service/ghg-projections.service';
+import { GhgProjectionsService } from './ghg-projections/ghg-projections.service';
 
 @Module({
   providers: [GhgEmissionsService, GhgProjectionsService]
