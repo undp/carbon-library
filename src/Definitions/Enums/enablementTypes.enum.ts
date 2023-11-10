8e9472798cf022b66da2783e14592df4c780b4aa
YathurshanT <yathurshant@xeptagon.com>
2023-11-10 14:13:31 +0530

    enablement type changes

--- src/Definitions/Enums/enablementTypes.enum.ts
+++ src/Definitions/Enums/enablementTypes.enum.ts
@@ -1,7 +1,5 @@
 export enum EnablementTypes {
   CapacityBuilding = 'Capacity Building',
-  // // InstitutionalArrangement = 'Institutional Arrangement',
-  // StakeholderFramework = 'Stakeholder Framework',
   TechnologyTransfer = 'Technology Transfer',
   Financial = 'Financial',
 }
@@ -13,5 +11,4 @@ export const enablementTypesAndValues = [
     col: 5,
   },
   { type: EnablementTypes.Financial.valueOf(), col: 4 },
-  // { type: EnablementTypes.TechnologyTransfer.valueOf(), col: 4 },
 ];
