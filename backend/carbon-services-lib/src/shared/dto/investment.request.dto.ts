926d7b958a6882ae72f11b9e8ce6a8991249a948
lochana <lochanaf@xeptagon.com>
2023-11-24 10:00:21 +0530

    initial developments

--- backend/carbon-services-lib/src/shared/dto/investment.request.dto.ts
+++ backend/carbon-services-lib/src/shared/dto/investment.request.dto.ts
@@ -5,83 +5,14 @@ import { InvestmentType } from "../enum/investment.type";
 import { InvestmentLevel } from "../enum/investment.level";
 import { InvestmentStream } from "../enum/investment.stream";
 import { ESGType } from "../enum/esg.type";
+import { InvestmentDto } from "./investment.dto";
 
-export class InvestmentRequestDto {
-
+export class InvestmentRequestDto extends InvestmentDto{
   @ApiProperty()
   @IsNotEmpty()
   @IsString()
   programmeId: string;
-
-  @IsNotEmpty()
-  @IsNumber()
-  @ApiProperty()
-  amount: number;
-
-  @ApiPropertyOptional({ enum: Instrument, isArray: true })
-  @IsEnum(Instrument, {
-      message: 'Invalid instrument type. Supported following values:' + Object.values(Instrument),
-      each: true,
-  })
-  @IsOptional()
-  @IsArray()
-  instrument: Instrument[];
-
-  @ApiPropertyOptional()
-  @ValidateIf(o => (o.instrument && o.instrument.indexOf(Instrument.LOAN) >= 0))
-  @IsNumber()
-  @IsOptional()
-  @IsNotEmpty()
-  interestRate?: number;
-
-  @ApiPropertyOptional()
-  @ValidateIf(o => (o.instrument && o.instrument.indexOf(Instrument.RESULT_BASED) >= 0))
-  @IsString()
-  @IsNotEmpty()
-  resultMetric?: string;
-
-  @ApiPropertyOptional()
-  @ValidateIf(o => (o.instrument && o.instrument.indexOf(Instrument.RESULT_BASED) >= 0))
-  @IsNumber()
-  @IsNotEmpty()
-  paymentPerMetric?: number;
-
-
-  @ApiPropertyOptional()
-  @ValidateIf(o => (o.instrument && o.instrument.indexOf(Instrument.OTHER) >= 0))
-  @IsString()
-  @IsNotEmpty()
-  comments?: string;
-
-
-  @ApiPropertyOptional({ enum: InvestmentType })
-  @IsEnum(InvestmentType, {
-      message: 'Invalid type. Supported following values:' + Object.values(InvestmentType)
-  })
-  @IsOptional()
-  type: InvestmentType;
-
-  @ApiPropertyOptional({ enum: InvestmentLevel })
-  @IsEnum(InvestmentLevel, {
-      message: 'Invalid level. Supported following values:' + Object.values(InvestmentLevel)
-  })
-  @IsOptional()
-  level: InvestmentLevel;
-
-  @ApiPropertyOptional({ enum: InvestmentStream })
-  @IsEnum(InvestmentStream, {
-      message: 'Invalid stream. Supported following values:' + Object.values(InvestmentStream)
-  })
-  @IsOptional()
-  stream: InvestmentStream;
-
-  @ApiPropertyOptional({ enum: ESGType })
-  @IsEnum(ESGType, {
-      message: 'Invalid esg type. Supported following values:' + Object.values(ESGType)
-  })
-  @IsOptional()
-  esgClassification: ESGType;
-
+  
   @ApiPropertyOptional()
   @IsArray()
   @IsInt({ each: true })
@@ -93,10 +24,5 @@ export class InvestmentRequestDto {
   @IsNumber({},{each: true})
   @Min(0, { each: true })
   percentage: number[];
-
-  @ApiProperty()
-  @IsNotEmpty()
-  @IsNumber()
-  toCompanyId: number;
 
 }
