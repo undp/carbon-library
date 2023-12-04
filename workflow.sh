42bd7c643dd1e5a4956b864d5112cc85e952fc1d
gayanath8 <gayanathr@xeptagon.com>
2023-12-04 12:14:55 +0530

    Enum or Config Changes Email Trigger

---
+++ workflow.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo -e "$(date +"%Y-%m-%d %H:%M:%S")\n\n........Enum Changes..........\n\n" > workflow.txt
+git diff HEAD~ -- HEAD -z ./backend/carbon-services-lib/src/shared/enum** >> workflow.txt
+echo -e "\n\n........Config Changes..........\n\n" >> workflow.txt
+git diff HEAD~ -- HEAD -z ./backend/carbon-services-lib/src/shared/configuration.ts >> workflow.txt
+
+timestamp=$(date "+%Y-%m-%d_%H-%M-%S")
+aws s3 cp workflow.txt s3://$AWS_BUCKET/workflow_txts_${timestamp}.txt
+CONTENT=$(base64 -w 0 workflow.txt)
+              
+RAW_MESSAGE="Subject: Libraries Changes
+MIME-Version: 1.0
+Content-Type: multipart/mixed; boundary=NextPart
+              
+--NextPart
+Content-Type: text/html
+              
+Please find the attachment for latest changes.
+              
+--NextPart
+Content-Type: text/plain; name=workflow.txt
+Content-Transfer-Encoding: base64
+Content-Disposition: attachment
+              
+$CONTENT
+              
+--NextPart--"
+              
+ENCODED_MESSAGE=$(echo -n "$RAW_MESSAGE" | base64)
+aws ses send-raw-email --source $FROM_EMAIL --destination $TO_EMAILS --raw-message "Data=$ENCODED_MESSAGE" 
