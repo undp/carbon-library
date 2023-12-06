456b63e3c463aac60d1e2dc99bcedb128067bc0d
Dhanuxeptagon <dhanushkapeiris@xeptagon.com>
2023-12-06 15:52:50 +0530

    issue fixes

--- src/Definitions/Enums/enablementTypes.enum.ts
+++ src/Definitions/Enums/enablementTypes.enum.ts
@@ -2,7 +2,7 @@ export enum EnablementTypes {
   CapacityBuilding = 'Capacity Building',
   TechnologyTransfer = 'Technology Transfer',
   Financial = 'Financial',
-  EnhancedTransparency= 'Enhanced Transparency'
+  EnhancedTransparency = 'Enhanced Transparency'
 }
 
 export const enablementTypesAndValues = [
@@ -12,4 +12,5 @@ export const enablementTypesAndValues = [
     col: 5,
   },
   { type: EnablementTypes.Financial.valueOf(), col: 4 },
+  { type: EnablementTypes.EnhancedTransparency.valueOf(), col: 5 },
 ];
