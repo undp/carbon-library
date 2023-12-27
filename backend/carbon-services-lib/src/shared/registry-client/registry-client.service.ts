744c678ceb00ac4c26221f7342a44b25d3ac2f79
gayanath8 <gayanathr@xeptagon.com>
2023-12-28 01:41:13 +0530

    Programme Create Sync Remove

--- backend/carbon-services-lib/src/shared/registry-client/registry-client.service.ts
+++ backend/carbon-services-lib/src/shared/registry-client/registry-client.service.ts
@@ -233,6 +233,7 @@ export class RegistryClientService {
         "proponentPercentage": programme.proponentPercentage,
         "programmeProperties": props,
         "creditEst": programme.creditEst,
+        "article6trade": true,
         "environmentalAssessmentRegistrationNo": programme.environmentalAssessmentRegistrationNo
       }
 
